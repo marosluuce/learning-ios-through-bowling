c0e111f32877e64234a811bb2bdbbf6f6c02ea68
Nathan Walker <earthworn@gmail.com>
2014-01-30 22:41:22 -0600

    Remove copyright notice.

--- LearningIosThroughBowling/BowlingGame.h
+++ LearningIosThroughBowling/BowlingGame.h
@@ -1,11 +1,3 @@
-//
-//  BowlingGame.h
-//  LearningIosThroughBowling
-//
-//  Created by Nathan Walker on 1/30/14.
-//  Copyright (c) 2014 Nathan Walker. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface BowlingGame : NSObject
--- LearningIosThroughBowling/BowlingGame.m
+++ LearningIosThroughBowling/BowlingGame.m
@@ -1,11 +1,3 @@
-//
-//  BowlingGame.m
-//  LearningIosThroughBowling
-//
-//  Created by Nathan Walker on 1/30/14.
-//  Copyright (c) 2014 Nathan Walker. All rights reserved.
-//
-
 #import "BowlingGame.h"
 
 @implementation BowlingGame
